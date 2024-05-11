e427d3bb540b9c1fc25d1856684a8443fa751a13
tampdph33277 <tampdph33277@fpt.edu.vn>
2024-05-11 14:24:03 +0700

    lan2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const InstagramUrlDirect = require('instagram-url-direct');
+const axios = require('axios');
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/download-instagram-image', async function(req, res, next) {
+    try {
+        const instagramUrl = req.query.url;
+        const imageUrl = await InstagramUrlDirect.getImageUrl(instagramUrl);
+        // Kiểm tra xem imageUrl có giá trị không trước khi tải ảnh
+        if (imageUrl) {
+            const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+            res.set('Content-Type', 'image/jpeg');
+            res.set('Content-Disposition', 'attachment; filename="image.jpg"');
+            res.send(imageResponse.data);
+        } else {
+            res.status(400).send('Không thể tải ảnh từ URL Instagram.');
+        }
+    } catch (error) {
+        console.error('Lỗi khi tải ảnh từ Instagram:', error);
+        res.status(500).send('Đã xảy ra lỗi khi tải ảnh từ Instagram.');
+    }
 });
 
 module.exports = router;
